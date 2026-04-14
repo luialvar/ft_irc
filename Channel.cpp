7b921559017f07d389a7fa05af94f25b2f7bde34
manuelfe <manuelfe@student.42malaga.com>
2026-04-14 18:33:22 +0200

    implementando metodos clase Channel

--- Channel.cpp
+++ Channel.cpp
@@ -54,3 +54,52 @@ void Channel::setMode(char mode)
 {
 	_modes.insert(mode);
 }
+
+void Channel::unsetMode(char mode)
+{
+	_modes.erase(mode);
+}
+
+bool Channel::isModeSet(char mode) const
+{
+	return std::find(_modes.begin(), _modes.end(), mode) != _modes.end();
+}
+
+bool Channel::isOperator(Client *client) const
+{
+	return std::find(_operators.begin(), _operators.end(), client) != _operators.end();
+}
+
+void Channel::addOperator(Client *client)
+{
+	if (!isOperator(client))
+		_operators.push_back(client);
+}
+
+void Channel::removeOperator(Client *client)
+{
+	std::vector<Client*>::iterator it = std::find(_operators.begin(), _operators.end(), client);
+	if (it != _operators.end())
+		_operators.erase(it);
+}
+
+void Channel::setKey(const std::string &key)
+{
+	_key = key;
+}
+
+void Channel::removeKey()
+{
+	_key.clear();
+}
+
+bool Channel::checkKey(const std::string &key) const
+{
+	return _key == key;
+}
+
+const std::string &Channel::getKey() const
+{
+	return _key;
+}
+
